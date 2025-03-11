df2da790de5359874529a7dfab8cc7eb9b0e77b3
ithiame <ithiame@agiltoo.fr>
2025-03-11 13:17:22 +0000

    🦋 Admin > Products - open the product in a new tab (bis) #1712

--- src/lib/components/ProductItem.svelte
+++ src/lib/components/ProductItem.svelte
@@ -2,6 +2,7 @@
 	import type { Product } from '$lib/types/Product';
 	import type { Picture } from '$lib/types/Picture';
 	import PictureComponent from './Picture.svelte';
+	import IconExternalNewWindowOpen from './icons/IconExternalNewWindowOpen.svelte';
 
 	export let picture: Picture | undefined;
 	export let product: Pick<Product, '_id' | 'name'>;
@@ -13,13 +14,19 @@
 <div class="flex flex-col text-center {className}">
 	<a
 		href="{isAdmin ? '/admin' : ''}/product/{product._id}"
-		target={isAdmin ? '_blank' : ''}
-		class="flex flex-col items-center"
+		class="flex flex-col items-center relative"
 	>
 		<PictureComponent {picture} class="block h-36" />
-		<span class="mt-2 line-clamp-3 text-ellipsis max-w-[192px] break-words hyphens-auto"
-			>{product.name}</span
-		>
+		{#if isAdmin}
+			<a class="" target={isAdmin ? '_blank' : ''} href="/admin/product/{product._id}">
+				<IconExternalNewWindowOpen
+					class="absolute top-0 right-0 bg-white p-1 rounded-full shadow-md h-8 w-auto"
+				/>
+			</a>
+		{/if}
+		<span class="mt-2 line-clamp-3 text-ellipsis max-w-[192px] break-words hyphens-auto">
+			{product.name}
+		</span>
 	</a>
 	<slot />
 </div>
