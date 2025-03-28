0900faf6b074972dbd37a60ac02cffdd77b5c418
ithiame <133223073+ithiame@users.noreply.github.com>
2025-03-28 20:58:16 +0000

    A be-BOP administrator cannot add translations for challenges #1588 (#1767)

---
+++ src/routes/(app)/admin[[hash=admin_hash]]/challenge/[id]/+layout.server.ts
@@ -0,0 +1,57 @@
+import { collections } from '$lib/server/database.js';
+import type { Order } from '$lib/types/Order';
+import type { Product } from '$lib/types/Product';
+import { error } from '@sveltejs/kit';
+import type { Filter } from 'mongodb';
+
+export async function load({ params }) {
+	const challenge = await collections.challenges.findOne({
+		_id: params.id
+	});
+
+	if (!challenge) {
+		throw error(404, 'Challenge not found');
+	}
+
+	const beginsAt = challenge.beginsAt;
+	const endsAt = challenge.endsAt;
+	const products = await collections.products
+		.find({})
+		.project<Pick<Product, 'name' | '_id'>>({ name: 1 })
+		.toArray();
+	const query: Filter<Order> = {
+		createdAt: {
+			$lt: endsAt,
+			$gt: beginsAt
+		},
+		status: 'paid'
+	};
+	if (challenge.productIds.length > 0) {
+		query['items.product._id'] = { $in: [...challenge.productIds] };
+	}
+	const orders = await collections.orders.find(query).sort({ createdAt: -1 }).toArray();
+
+	return {
+		challenge,
+		beginsAt,
+		endsAt,
+		products,
+		orders: orders.map((order) => ({
+			_id: order._id,
+			payments: order.payments.map((payment) => ({
+				id: payment._id.toString(),
+				status: payment.status,
+				method: payment.method
+			})),
+			number: order.number,
+			createdAt: order.createdAt,
+			status: order.status,
+			notes:
+				order.notes?.map((note) => ({
+					content: note.content,
+					createdAt: note.createdAt
+				})) || [],
+			currencySnapshot: order.currencySnapshot
+		}))
+	};
+}
