df3a2913741d4186f6cc5671aca8a117b4ba37c7
ithiame <ithiame@agiltoo.fr>
2025-04-01 13:27:51 +0000

    html escape

--- src/routes/(app)/schedule/[id]/rss.xml/+server.ts
+++ src/routes/(app)/schedule/[id]/rss.xml/+server.ts
@@ -11,24 +11,18 @@ export const GET = async ({ params }) => {
 	}
 
 	let rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n<channel>\n`;
-	rssFeed += `<title>${schedule.name.replaceAll(/</g, '&lt;').replaceAll(/>/g, '&gt;')}</title>\n`;
-	rssFeed += `<link>${ORIGIN}/schedule/${schedule._id
-		.replaceAll(/</g, '&lt;')
-		.replaceAll(/>/g, '&gt;')}</link>\n`;
+	rssFeed += `<title>${schedule.name.replaceAll(/</g, '&lt;')}</title>\n`;
+	rssFeed += `<link>${ORIGIN}/schedule/${schedule._id.replaceAll(/</g, '&lt;')}</link>\n`;
 	rssFeed += `<description>List of events</description>\n`;
 	rssFeed += `<lastBuildDate>${new Date().toUTCString()}</lastBuildDate>\n`;
 	rssFeed += `<language>fr</language>\n`;
 
 	schedule.events.forEach((event) => {
 		rssFeed += `<item>\n`;
-		rssFeed += `  <title>${event.title
-			.replaceAll(/</g, '&lt;')
-			.replaceAll(/>/g, '&gt;')}</title>\n`;
-		rssFeed += `<link>${ORIGIN}/schedule/${schedule._id
-			.replaceAll(/</g, '&lt;')
-			.replaceAll(/>/g, '&gt;')}</link>\n`;
+		rssFeed += `  <title>${event.title.replaceAll(/</g, '&lt;')}</title>\n`;
+		rssFeed += `<link>${ORIGIN}/schedule/${schedule._id.replaceAll(/</g, '&lt;')}</link>\n`;
 		rssFeed += `  <description>${
-			event.shortDescription?.replaceAll(/</g, '&lt;').replaceAll(/>/g, '&gt;') || ''
+			event.shortDescription?.replaceAll(/</g, '&lt;') || ''
 		}</description>\n`;
 		rssFeed += `  <pubDate>${format(
 			event.beginsAt,
