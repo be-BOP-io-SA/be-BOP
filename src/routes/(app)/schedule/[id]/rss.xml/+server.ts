98132a8d872bfa73f76a7d083c015b0cf56df7a4
ithiame <ithiame@agiltoo.fr>
2025-04-02 12:59:03 +0000

    add guid to rss and change pubdate to updateAt date

--- src/routes/(app)/schedule/[id]/rss.xml/+server.ts
+++ src/routes/(app)/schedule/[id]/rss.xml/+server.ts
@@ -1,7 +1,7 @@
 import { ORIGIN } from '$env/static/private';
 import { collections } from '$lib/server/database';
 import { error } from '@sveltejs/kit';
-import { format } from 'date-fns';
+import { addMinutes, format } from 'date-fns';
 
 export const GET = async ({ params }) => {
 	const schedule = await collections.schedules.findOne({ _id: params.id });
@@ -10,7 +10,7 @@ export const GET = async ({ params }) => {
 		throw error(404, 'schedule not found');
 	}
 
-	let rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n<channel>\n`;
+	let rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">\n<channel>\n`;
 	rssFeed += `<title>${schedule.name.replaceAll(/</g, '&lt;')}</title>\n`;
 	rssFeed += `<link>${ORIGIN}/schedule/${schedule._id.replaceAll(/</g, '&lt;')}/rss.xml</link>\n`;
 	rssFeed += `<description>List of events</description>\n`;
@@ -19,19 +19,19 @@ export const GET = async ({ params }) => {
 	rssFeed += `<atom:link href="${ORIGIN}/schedule/${schedule._id.replaceAll(
 		/</g,
 		'&lt;'
-	)}/rss.xml" rel="self" type="application/rss+xml" xmlns:atom="http://www.w3.org/2005/Atom"/>\n`;
-	schedule.events.forEach((event) => {
+	)}/rss.xml" rel="self" type="application/rss+xml"/>\n`;
+	schedule.events.forEach((event, index) => {
 		rssFeed += `<item>\n`;
 		rssFeed += `  <title>${event.title.replaceAll(/</g, '&lt;')}</title>\n`;
 		rssFeed += `<link>${ORIGIN}/schedule/${schedule._id.replaceAll(/</g, '&lt;')}</link>\n`;
 		rssFeed += `  <description>${
-			event.shortDescription?.replaceAll(/</g, '&lt;') || ''
+			(event.shortDescription ?? 'description coming soon...').replaceAll(/</g, '&lt;') || ''
 		}</description>\n`;
 		rssFeed += `  <pubDate>${format(
-			schedule.updatedAt,
+			addMinutes(schedule.updatedAt, index),
 			"EEE, dd MMM yyyy HH:mm:ss 'GMT'"
 		)}</pubDate>\n`;
-		rssFeed += `<guid isPermaLink="false">${event.slug}</guid>\n`;
+		rssFeed += `<guid isPermaLink="true">${ORIGIN}/schedule/${schedule._id.replaceAll(/</g, '&lt;')}/event/${event.slug}</guid>\n`;
 		rssFeed += `</item>\n`;
 	});
 
