806924f14abc2cb6e6ed96e1b46353a8174f3be9
ithiame <ithiame@agiltoo.fr>
2025-04-02 08:48:04 +0000

    make admin great

--- src/routes/(app)/admin[[hash=admin_hash]]/+page.svelte
+++ src/routes/(app)/admin[[hash=admin_hash]]/+page.svelte
@@ -1,3 +1,114 @@
-<h1 class="text-3xl">Home</h1>
+<div class="container">
+	<h1>Back-office home</h1>
+	<p>
+		Welcome on be-BOP back-office! From here, you'll be able to manage, configure, and monitor your
+		be-BOP.
+	</p>
 
-Soon, a lot of awesome things will be here.
+	<h2>A word from your administrator :</h2>
+	<p>
+		<em
+			>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum at justo eu
+			dapibus. Suspendisse lacinia lacus nec ante euismod, non pulvinar nunc blandit..."</em
+		>
+	</p>
+
+	<h2>be-BOP version & updates</h2>
+
+	<h3>Version check</h3>
+	<p>
+		You're currently using this version of be-BOP : <span class="code"
+			>2b4316c464652d62c7a06f17902172eb68218b8</span
+		>
+	</p>
+	<p>
+		The last version on official repo is : <span class="code"
+			>2b4316c464652d62c7a06f17902172eb68218b8</span
+		>
+	</p>
+
+	<p class="check">✅ Your be-BOP is from an official build</p>
+	<p class="check">✅ Your be-BOP version is up-to-date</p>
+
+	<div class="updates">
+		<h3>Last releases</h3>
+		<a href="#" class="check-link">>>> Check here for more updates</a>
+
+		<table class="release-table">
+			<thead>
+				<tr>
+					<th>Release date</th>
+					<th>Object</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>08/03/2025</td>
+					<td>Feature "Leaderboard"</td>
+				</tr>
+				<tr>
+					<td>06/03/2025</td>
+					<td>Various bugfixes (minor)</td>
+				</tr>
+				<tr>
+					<td>05/03/2025</td>
+					<td>Add VAT and delivery fees on Transactions > Reporting</td>
+				</tr>
+				<tr>
+					<td>04/03/2025</td>
+					<td>Fix on transaction confirmation threshold</td>
+				</tr>
+				<tr>
+					<td>02/03/2025</td>
+					<td>Various documentation</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<a href="#" class="check-link">>>> Check for more</a>
+	</div>
+
+	<h2>Documentation</h2>
+
+	<p>Select your language :</p>
+	<div class="languages">
+		<img src="flags/en.png" alt="English" class="flag" />
+		<img src="flags/fr.png" alt="French" class="flag" />
+		<img src="flags/it.png" alt="Italian" class="flag" />
+		<img src="flags/nl.png" alt="Dutch" class="flag" />
+		<img src="flags/es.png" alt="Spanish" class="flag" />
+	</div>
+
+	<p>Select your topic :</p>
+	<ul class="doc-list">
+		<li>00 - initialize beBOP.md</li>
+		<li>FAQ.md</li>
+		<li>VAT-configuration.md</li>
+		<li>admin-reporting.md</li>
+		<li>age-restriction.md</li>
+		<li>back-office-access.md</li>
+		<li>bebop-manifesto.md</li>
+		<li>bulk-picture-name.md</li>
+		<li>challenge-widget.md</li>
+		<li>configuration-bitcoin-nodeless.md</li>
+		<li>countdown-widget.md</li>
+		<li>customise-cart-checkout-order-with-CMS.md</li>
+		<li>delivery-management.md</li>
+		<li>downloadable-files.md</li>
+		<li>form-widget.md</li>
+		<li>gallery-widget.md</li>
+		<li>language-configuration.md</li>
+		<li>layout-design.md</li>
+		<li>leaderboard-widget.md</li>
+		<li>mobile-display.md</li>
+		<li>nostr-configuration.md</li>
+		<li>paypal-configuration.md</li>
+		<li>phoenixd-configuration.md</li>
+		<li>physical-shop.md</li>
+		<li>picture-option.md</li>
+		<li>point-of-sale-options.md</li>
+		<li>pos-touch-screen.md</li>
+		<li>price-product-bulk-change.md</li>
+		<li>product-variation.md</li>
+	</ul>
+</div>
