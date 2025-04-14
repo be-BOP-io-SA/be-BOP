997a5843d3d3204da0dd12433b9d564eb4c49ef9
ithiame <ithiame@agiltoo.fr>
2025-04-14 14:38:59 +0000

    fix suggested changes

--- src/routes/(app)/docs/[lang]/[filename]/+server.ts
+++ src/routes/(app)/docs/[lang]/[filename]/+server.ts
@@ -1,15 +1,46 @@
+import { rootDir } from '$lib/server/root-dir';
 import { error } from '@sveltejs/kit';
 import fs from 'fs';
-import path from 'path';
+import { join } from 'path';
+import { z } from 'zod';
 
 export async function GET({ params }) {
-	const { lang, filename } = params;
-	const filePath = path.resolve('docs', lang, filename);
+	const docsDir = join(rootDir, 'docs');
 
-	if (!fs.existsSync(filePath)) {
+	const availableLangs = fs
+		.readdirSync(docsDir)
+		.filter((folder) => fs.statSync(join(docsDir, folder)).isDirectory());
+
+	const langSchema = z.enum(availableLangs as [string, ...string[]]);
+	let lang: string;
+
+	try {
+		lang = langSchema.parse(params.lang);
+	} catch {
+		throw error(400, 'Invalid language');
+	}
+
+	const folderPath = join(docsDir, lang);
+
+	const availableFiles = fs
+		.readdirSync(folderPath)
+		.filter((file) => file.endsWith('.md') && fs.statSync(join(folderPath, file)).isFile());
+
+	const filenameSchema = z.enum(availableFiles as [string, ...string[]]);
+	let filename: string;
+
+	try {
+		filename = filenameSchema.parse(params.filename);
+	} catch {
 		throw error(404, 'File not found');
 	}
 
+	const filePath = join(folderPath, filename);
+
 	const content = fs.readFileSync(filePath, 'utf-8');
-	return new Response(content, { status: 200, headers: { 'Content-Type': 'text/plain' } });
+
+	return new Response(content, {
+		status: 200,
+		headers: { 'Content-Type': 'text/plain' }
+	});
 }
