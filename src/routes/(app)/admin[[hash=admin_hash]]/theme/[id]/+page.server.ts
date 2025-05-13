2205b2ae7b0d05fb6be6b70670c7766f13c3c82e
ithiame <ithiame@agiltoo.fr>
2025-05-13 12:07:02 +0000

    Duplicate dark to light / light to dark

--- src/routes/(app)/admin[[hash=admin_hash]]/theme/[id]/+page.server.ts
+++ src/routes/(app)/admin[[hash=admin_hash]]/theme/[id]/+page.server.ts
@@ -4,10 +4,12 @@ import type { JsonObject } from 'type-fest';
 import { set } from '$lib/utils/set';
 import { collections } from '$lib/server/database';
 import { adminPrefix } from '$lib/server/admin';
-import type { Theme } from '$lib/types/Theme';
+import { themeFormStructure, type Theme } from '$lib/types/Theme';
 import type { Timestamps } from '$lib/types/Timestamps';
 import { increaseThemeChangeNumber, themeValidator } from '$lib/server/theme';
 import { ObjectId } from 'mongodb';
+import { object } from 'zod';
+import { get } from '$lib/utils/get';
 
 export async function load({ params }) {
 	const theme = await collections.themes.findOne({ _id: new ObjectId(params.id) });
@@ -25,7 +27,7 @@ export async function load({ params }) {
 }
 
 export const actions: Actions = {
-	default: async ({ request, params }) => {
+	update: async ({ request, params }) => {
 		const formData = await request.formData();
 		const json: JsonObject = {};
 
@@ -34,6 +36,100 @@ export const actions: Actions = {
 		}
 
 		const theme = themeValidator.parse(json) satisfies Omit<Theme, '_id' | keyof Timestamps>;
+
+		await collections.themes.updateOne(
+			{ _id: new ObjectId(params.id) },
+			{
+				$set: {
+					...theme,
+					updatedAt: new Date()
+				}
+			}
+		);
+
+		await increaseThemeChangeNumber();
+	},
+	duplicateToDark: async ({ request, params }) => {
+		const formData = await request.formData();
+		const json: JsonObject = {};
+
+		for (const [key, value] of formData) {
+			set(json, key, value);
+		}
+		const jsonToDuplicate: JsonObject = {};
+		jsonToDuplicate.name = json.name;
+		for (const [section, fields] of Object.entries(themeFormStructure)) {
+			for (const field of fields.elements) {
+				const key = `${section}.${field.name}`;
+				if (key.endsWith('color') || key.endsWith('Color')) {
+					const darkPath = `${key}.dark`;
+					const lightPath = `${key}.light`;
+
+					const lightValue = get(json, lightPath);
+
+					if (lightValue !== undefined) {
+						set(jsonToDuplicate, lightPath, lightValue);
+						set(jsonToDuplicate, darkPath, lightValue);
+					}
+				} else {
+					const value = get(json, key);
+					if (value !== undefined) {
+						set(jsonToDuplicate, key, value);
+					}
+				}
+			}
+		}
+		const theme = themeValidator.parse(jsonToDuplicate) satisfies Omit<
+			Theme,
+			'_id' | keyof Timestamps
+		>;
+
+		await collections.themes.updateOne(
+			{ _id: new ObjectId(params.id) },
+			{
+				$set: {
+					...theme,
+					updatedAt: new Date()
+				}
+			}
+		);
+
+		await increaseThemeChangeNumber();
+	},
+	duplicateToLight: async ({ request, params }) => {
+		const formData = await request.formData();
+		const json: JsonObject = {};
+
+		for (const [key, value] of formData) {
+			set(json, key, value);
+		}
+		const jsonToDuplicate: JsonObject = {};
+		jsonToDuplicate.name = json.name;
+		for (const [section, fields] of Object.entries(themeFormStructure)) {
+			for (const field of fields.elements) {
+				const key = `${section}.${field.name}`;
+				if (key.endsWith('color') || key.endsWith('Color')) {
+					const darkPath = `${key}.dark`;
+					const lightPath = `${key}.light`;
+
+					const darkValue = get(json, darkPath);
+
+					if (darkValue !== undefined) {
+						set(jsonToDuplicate, lightPath, darkValue);
+						set(jsonToDuplicate, darkPath, darkValue);
+					}
+				} else {
+					const value = get(json, key);
+					if (value !== undefined) {
+						set(jsonToDuplicate, key, value);
+					}
+				}
+			}
+		}
+		const theme = themeValidator.parse(jsonToDuplicate) satisfies Omit<
+			Theme,
+			'_id' | keyof Timestamps
+		>;
 
 		await collections.themes.updateOne(
 			{ _id: new ObjectId(params.id) },
