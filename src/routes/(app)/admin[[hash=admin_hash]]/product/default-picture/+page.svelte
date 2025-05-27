4751ea8630ef8811491e4724250072b863cd9eef
ithiame <ithiame@agiltoo.fr>
2025-05-27 16:42:35 +0000

    🦋 Allow to define default product picture in admin for product without picture #1809

---
+++ src/routes/(app)/admin[[hash=admin_hash]]/product/default-picture/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import DEFAULT_PICTURE from '$lib/assets/default-picture.svg';
+
+	let uploading = false;
+</script>
+
+<h1 class="text-3xl">Add a default picture for product</h1>
+
+<form method="post" class="flex flex-col gap-4" enctype="multipart/form-data">
+	Current picture
+	<!-- svelte-ignore a11y-img-redundant-alt -->
+	<img srcset={DEFAULT_PICTURE} alt="default-picture" class="w-24 h-24" />
+
+	<label>
+		File
+		<input type="file" class="block" required name="file" accept=".svg,image/svg+xml" />
+	</label>
+
+	<input type="submit" class="btn btn-gray self-start" value="Add" disabled={uploading} />
+</form>
